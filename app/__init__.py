8a99f6857bf944298a8916010f442b6ddf43e96f
kennethrioja <59597207+kennethrioja@users.noreply.github.com>
2024-06-14 20:47:21 +0200

    feat(db): User is working fully on db

--- app/__init__.py
+++ app/__init__.py
@@ -8,34 +8,6 @@ from flask_httpauth import HTTPBasicAuth
 db = SQLAlchemy()
 migrate = Migrate()
 auth = HTTPBasicAuth()
-users = [
-    {
-        "joueur": 1,
-        "email": "first@example.com",
-        "firstName": "John",
-        "lastName": "Doe",
-    }
-]
-logs = [
-    {
-        "id": 1,
-        "joueur": 26051990,
-        "timestamp": "2023-04-18T10:12:51.832Z",
-        "sequence": 1,
-        "nomSalle": "consentement",
-        "action": "connexion",
-        "reponseJoueur": "",
-        "typeAction": "systeme",
-        "erreurJoueur": ""
-    }
-]
-
-# GLOBAL VARIABLES
-ERR_JSON = "Not a json object"
-ERR_USERS_NFIELD = "Users - Missing required fields (email, firstName, lastName)"
-ERR_USERS_KEYSYNTAX = "Users - Incorrect key syntax (email, firstName, lastName)"
-ERR_USERS_EMAILSYNTAX = "Users - Incorrect email syntax"
-ERR_USERS_NAMELEN = "Users - firstName and lastName values must be at least 2 characters long"
 
 
 def create_app(config_class=Config):
@@ -43,12 +15,12 @@ def create_app(config_class=Config):
     app.config.from_object(Config)
 
     db.init_app(app)
-    migrate.init_app(app, db)
+    migrate.init_app(app, db, render_as_batch=True)
 
     from app.api import bp as api_bp
     app.register_blueprint(api_bp, url_prefix='/tsadk/api')
 
-    from app.errors import bp as errors_bp
+    from app.api.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
 
     return app
--- app/app.py
+++ app/app.py
@@ -1,4 +1,12 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.api.models import User, Log
+
+
+@app.api.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Log': Log}
 
 
 if __name__ == '__main__':
